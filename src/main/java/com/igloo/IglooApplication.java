d79a7b7e6d83f15ae2d5f81c89e0816e72aad917
leehyokyun <gyrbs22@gmail.com>
2026-05-18 20:34:26 +0900

    env variables test

--- src/main/java/com/igloo/IglooApplication.java
+++ src/main/java/com/igloo/IglooApplication.java
@@ -1,11 +1,21 @@
 package com.igloo;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IglooApplication {
+
+    @PostConstruct
+    public void check() {
+        System.out.println("DB_PASSWORD = " + System.getenv("DB_PASSWORD"));
+    }
+
     public static void main(String[] args) {
+
+
+
         SpringApplication.run(IglooApplication.class, args);
     }
 }
