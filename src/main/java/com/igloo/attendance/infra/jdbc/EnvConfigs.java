fe640872b396b5a2616b704193f774b4c24046a4
leehyokyun <gyrbs22@gmail.com>
2026-05-18 20:58:00 +0900

    env variables test #4

---
+++ src/main/java/com/igloo/attendance/infra/jdbc/EnvConfigs.java
@@ -0,0 +1,19 @@
+package com.igloo.attendance.infra.jdbc;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class EnvConfigs {
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl(System.getenv("DATABASE_URL"));
+        ds.setUsername(System.getenv("DATABASE_USER"));
+        ds.setPassword(System.getenv("DATABASE_PASSWORD"));
+        return ds;
+    }
+}
