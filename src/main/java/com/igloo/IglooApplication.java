1d6bee26b46e51b5014c644b9e47936a3b9b9374
leehyokyun <gyrbs22@gmail.com>
2026-06-06 10:40:41 +0900

    added EnableScheduling

--- src/main/java/com/igloo/IglooApplication.java
+++ src/main/java/com/igloo/IglooApplication.java
@@ -3,7 +3,9 @@ package com.igloo;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class IglooApplication {
     public static void main(String[] args) {
